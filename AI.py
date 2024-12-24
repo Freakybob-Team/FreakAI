7877e53b6b9dc9dcbfe674dfc1557d0b62c720c4
Wish <100239673+wish13yt@users.noreply.github.com>
2024-12-24 11:18:15 -0600

    test

--- AI.py
+++ AI.py
@@ -1,21 +1,31 @@
+import urllib.request, json 
+with urllib.request.urlopen("https://github.com/Freakybob-Team/FreakAI/prompts.json?raw=true") as url:
+    global data
+    data = json.load(url)
 print("FreakAI: Hello, I am FreakAI!")
 def ask():
     global message
     message = input("")
 ask()
 while True:
-    if (message == "Are you freaky?"):
-        print("FreakAI: Yes, I am freaky, as I am a large language model from Freakybob Team.")
+    if (message in data):
+        print(data[message])
         ask()
-    if (message == "boom" or "Boom"):
-        print("FreakAI: No more Malaysia")
-        ask()
-    if (message == "freaky" or "Freaky"):
-        print("FreakAI: I'm currently with your mother.")
-        ask()
-    if (message == "dog"):
-        print("FreakAI: llllllllldexm                       ")
-        ask()
+    # if (message == "boom" or "Boom"):
+    #     print("FreakAI: No more Malaysia")
+    #     ask()
+    # if (message == "freaky" or "Freaky"):
+    #     print("FreakAI: I'm currently with your mother.")
+    #     ask()
+    # if (message == "dog"):
+    #     print("FreakAI: llllllllldexm                       ")
+    #     ask()
+    # if (message == "boom" or "Boom"):
+    #     print("FreakAI: No more Malaysia")
+    #     ask()
+    # if (message == "freaky" or "Freaky"):
+    #     print("I'm currently with your mother.")
+    #     ask()
     else:
         print("FreakAI: I'm sorry, but I do not understand what you are saying. Try again later!")
         ask()
