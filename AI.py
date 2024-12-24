ee5cb85648d59ef2fe666775ad2aa8b16c78267a
Wish <100239673+wish13yt@users.noreply.github.com>
2024-12-24 11:04:59 -0600

    Update AI.py

--- AI.py
+++ AI.py
@@ -6,7 +6,7 @@ if (message == "Are you freaky?"):
     print("FreakAI: Yes, I am freaky, as I am a large language model from Freakybob Team.")
     ask()
 elif (message == "boom"):
-    print("No more malaysia")
+    print("FreakAI: No more malaysia")
     ask()
 else:
     print("FreakAI: I'm sorry, but I do not understand what you are saying. Try again later!")
